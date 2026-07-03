ae22627793d2bb1e4759e0087a558cfdfd6ed900
rif42 <rifkyariya42@gmail.com>
2026-07-03 09:53:36 +0700

    msgs

---
+++ messages.js
@@ -0,0 +1,19 @@
+const messages = [
+  "happy birthday babe",
+  "i love you so much",
+  "i need your presence",
+  "i need your attention",
+  "life would be dull without you",
+  "i always want to spend time with you",
+  "please take care of yourself",
+  "i know you have scars",
+  "im trying my best to help you heal",
+  "just know that im always here, ready for you",
+  "i never want you to struggle alone",
+  "i wish we can be together, forever",
+  "i love you so much",
+];
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = messages;
+}
